068f2e7f72acc78d8ccd1e5e18678a52daf1f07b
Vlad Zolotarov <vladz@cloudius-systems.com>
2013-12-30 18:49:46 +0200

    Fixed ring_spsc
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

--- include/lockfree/ring.hh
+++ include/lockfree/ring.hh
@@ -14,26 +14,39 @@
 #include <atomic>
 #include <sched.hh>
 #include <arch.hh>
+#include <ilog2.hh>
+#include "debug.hh"
 
 //
 // spsc ring of fixed size
 //
-template<class T, unsigned MaxSize>
+template<class T>
 class ring_spsc {
 public:
-    ring_spsc(): _begin(0), _end(0) { }
+    ring_spsc(unsigned max_size): _begin(0), _end(0), _max_size(max_size),
+                                  _max_size_mask(max_size - 1)
+    {
+        assert(is_power_of_two(_max_size));
+        _ring = new T[_max_size];
+    }
+
+    ~ring_spsc()
+    {
+        delete[] _ring;
+    }
 
     bool push(const T& element)
     {
         unsigned end = _end.load(std::memory_order_relaxed);
-        unsigned beg = _begin.load(std::memory_order_relaxed);
 
-        if (end - beg >= MaxSize) {
+        // Consider making it a DEBUG_ASSERT() since it's going to cause a
+        // false sharing on a "_begin" cache line
+        if (size() >= _max_size) {
             return false;
         }
 
-        _ring[end % MaxSize] = element;
-        _end.store(end + 1, std::memory_order_release);
+        _ring[end & _max_size_mask] = element;
+        _end.store(end + 1, std::memory_order_relaxed);
 
         return true;
     }
@@ -41,19 +54,32 @@ public:
     bool pop(T& element)
     {
         unsigned beg = _begin.load(std::memory_order_relaxed);
-        unsigned end = _end.load(std::memory_order_acquire);
 
-        if (beg >= end) {
+        // Consider making it a DEBUG_ASSERT() since it's going to cause a
+        // false sharing on a "_begin" cache line
+        if (empty()) {
             return false;
         }
 
-        element = _ring[beg % MaxSize];
+        element = _ring[beg & _max_size_mask];
         _begin.store(beg + 1, std::memory_order_relaxed);
 
         return true;
     }
 
-    unsigned size() {
+    bool empty() const {
+        return size() == 0;
+    }
+
+    T& front() const {
+        DEBUG_ASSERT(!empty(), "calling front() on an empty queue!");
+
+        unsigned beg = _begin.load(std::memory_order_relaxed);
+
+        return _ring[beg & _max_size_mask];
+    }
+
+    unsigned size() const {
         unsigned end = _end.load(std::memory_order_relaxed);
         unsigned beg = _begin.load(std::memory_order_relaxed);
 
@@ -63,7 +89,9 @@ public:
 private:
     std::atomic<unsigned> _begin CACHELINE_ALIGNED;
     std::atomic<unsigned> _end CACHELINE_ALIGNED;
-    T _ring[MaxSize];
+    const unsigned _max_size;
+    const unsigned _max_size_mask;
+    T* _ring;
 };
 
 //

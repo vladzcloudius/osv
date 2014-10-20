ca262bb5b7dd10f810ba6a58ce40ca93e9c4fa74
Vlad Zolotarov <vladz@cloudius-systems.com>
2014-10-20 19:28:29 +0300

    dynamic_thread_priority: comment the printouts
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

--- include/osv/dynamic_thread_priority.hh
+++ include/osv/dynamic_thread_priority.hh
@@ -91,10 +91,10 @@ public:
                 rc = prio_up;
             }
 
-            printf("CPU[%d]: %s: idle %d, cur_prio * 100000 %d\n",
+            /*printf("CPU[%d]: %s: idle %d, cur_prio * 100000 %d\n",
                    sched::current_cpu->id, current->name().c_str(),
                    average_idle_time,
-                   (u64)(cur_prio * 100000));
+                   (u64)(cur_prio * 100000));*/
 
             _last_idle_clock = cur_idle_clock;
             _start = osv::clock::uptime::now();
